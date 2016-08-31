ab0299678b434fde695c7d40f757fafe036e133d
Basil Suter <git@nadar.io>
2016-08-31 09:52:22 +0200

    merge crawler module #972

---
+++ admin/messages/de/crawleradmin.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'crawler' => 'Crawler',
+    'crawler_indexing' => 'Indexierung',
+
+    //builderindex
+    'crawler_index' => 'Seiten Index',
+    'builderindex_url' => 'Url',
+    'builderindex_title' => 'Bezeichnung',
+    'builderindex_language_info' => 'Sprachinfo',
+    'builderindex_content' => 'Inhalt',
+    'builderindex_url_found' => 'URL auf folgender Seite gefunden',
+
+    //index
+    'crawler_builderindex' => 'Zwischenspeicher',
+    'index_url' => 'Url',
+    'index_title' => 'Bezeichnung',
+    'index_language_info' => 'Sprachinfo',
+    'index_content' => 'Inhalt',
+    'index_url_found' => 'URL auf folgender Seite gefunden',
+
+];
---
+++ admin/messages/en/crawleradmin.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'crawler' => 'Crawler',
+    'crawler_indexing' => 'Indexing',
+
+    //builderindex
+    'crawler_index' => 'Page index',
+    'builderindex_url' => 'Url',
+    'builderindex_title' => 'Title',
+    'builderindex_language_info' => 'Language info',
+    'builderindex_content' => 'Content',
+    'builderindex_url_found' => 'Url found on Page',
+
+    //index
+    'crawler_builderindex' => 'Page cache',
+    'index_url' => 'Url',
+    'index_title' => 'Title',
+    'index_language_info' => 'Language info',
+    'index_content' => 'Content',
+    'index_url_found' => 'Url found on Page',
+
+];
