cf94bfe48541374af3a349a75f28a3360fdf1c2b
Basil Suter <git@nadar.io>
2019-05-27 16:52:40 +0200

    add migration files test

--- tests/GenericFilesTest.php
+++ tests/GenericFilesTest.php
@@ -3,18 +3,25 @@
 namespace luya\crawler\tests;
 
 use luya\testsuite\traits\MessageFileCompareTrait;
+use luya\testsuite\traits\MigrationFileCheckTrait;
 
 /**
  * Message File Compare Trait.
  * @author nadar
  *
  */
-class MessageFileTest extends CrawlerTestCase
+class GenericFilesTest extends CrawlerTestCase
 {
     use MessageFileCompareTrait;
+    use MigrationFileCheckTrait;
     
     public function testFiles()
     {
         $this->compareMessages(__DIR__ . '/../src/admin/messages', 'en');
+    }
+
+    public function testMigrations()
+    {
+        $this->checkMigrationFolder('@crawleradmin/migrations');
     }
 }
