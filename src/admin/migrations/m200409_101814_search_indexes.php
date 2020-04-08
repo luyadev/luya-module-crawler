e5d4ac42272e9c5b7c4b4db041ab2eaf5e9f3c06
Basil <git@nadar.io>
2020-04-08 11:38:51 +0200

    remove index

--- src/admin/migrations/m200409_101814_search_indexes.php
+++ src/admin/migrations/m200409_101814_search_indexes.php
@@ -14,12 +14,6 @@ class m200409_101814_search_indexes extends Migration
     {
         $this->createIndex('language_info' , 'crawler_index', ['language_info']);
         $this->createIndex('group' , 'crawler_index', ['group']);
-
-        if ($this->db->driverName == 'mysql') {
-            $time = $this->beginCommand('create fulltext index on content');
-            $this->db->createCommand('CREATE FULLTEXT INDEX content ON crawler_index ( content );')->execute();
-            $this->endCommand($time);
-        }
     }
 
     /**
@@ -29,9 +23,5 @@ class m200409_101814_search_indexes extends Migration
     {
         $this->dropIndex('language_info' , 'crawler_index');
         $this->dropIndex('group' , 'crawler_index');
-
-        if ($this->db->driverName == 'mysql') {
-            $this->dropIndex('content' , 'crawler_index');
-        }
     }
 }
