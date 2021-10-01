312147990009c4e55832d48755d8037c36f66c56
dennisgon <dennisgon69@hotmail.com>
2021-10-02 04:51:06 +0700

    add translation in indonesian languange

---
+++ src/admin/messages/id/crawleradmin.php
@@ -0,0 +1,52 @@
+<?php
+kembali [
+    'crawler' => 'Perayap',
+    'crawler_indexing' => 'Pengindeksan',
+    'crawler_analytics' => 'Analitik',
+    'crawler_analytics_queries' => 'Kueri Penelusuran',
+
+    //indeks pembangun
+    'crawler_index' => 'Indeks',
+    'builderindex_url' => 'URL',
+    'builderindex_title' => 'Judul',
+    'builderindex_language_info' => 'Info bahasa',
+    'builderindex_content' => 'Konten',
+    'builderindex_url_found' => 'URL ditemukan di halaman',
+
+    //indeks
+    'crawler_builderindex' => 'Cache halaman',
+    'index_url' => 'URL',
+    'index_title' => 'Judul',
+    'index_language_info' => 'Info bahasa',
+    'index_content' => 'Konten',
+    'index_url_found' => 'URL ditemukan di halaman',
+
+    // 1.0.6
+
+    'added_to_index' => 'Ditambahkan ke Indeks',
+    'last_update' => 'Pembaruan Terakhir',
+    'dashboard_title' => 'Kueri terbaru tanpa hasil',
+
+    // 2.0
+
+    'link_url' => 'Tautan-Tarket',
+    'link_url_found_on_page' => 'Sumber',
+    'link_title' => 'Teks Tautan',
+    'link_is_ignored' => 'Diabaikan',
+    'link_response_status' => 'Status Tanggapan',
+    'link_created_at' => 'Dibuat di',
+    'link_updated_at' => 'Diperbarui pada',
+    'done_button_label' => 'Terselesaikan',
+    'done_button_success' => 'Tautan telah dihapus dari Daftar.',
+    'crawler_menu_links' => 'Tautan',
+    'crawler_menu_stats' => 'Statistik',
+    'stats_from' => 'Dari',
+    'stats_to' => 'Ke',
+    'stats_searches_title' => 'Penelusuran',
+    'stats_no_results_title' => 'Kueri tanpa Hasil',
+    'stats_most_suggestions_title' => 'Kueri yang paling sering disarankan',
+    'stats_no_data' => 'Data tidak tersedia',
+    'legend_total' => 'Total',
+    'legend_results' => 'Tidak Ada Hasil',
+    'legend_no_results' => 'Dengan Hasil',
+];
